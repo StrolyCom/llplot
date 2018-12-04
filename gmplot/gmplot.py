5c9044f0ed9a119388011027480bcfbbc02ffc73
Enrique Arriaga <enrique@stroly.jp>
2018-12-04 11:32:37 +0900

    fix format

--- gmplot/gmplot.py
+++ gmplot/gmplot.py
@@ -356,7 +356,7 @@ class GoogleMapPlotter(object):
 
     # TODO: Add support for mapTypeId: google.maps.MapTypeId.SATELLITE
     def write_map(self,  f):
-        f.write('\t\tvar baseLayer = L.tileLayer(%f, {%f, mapid: "streets"});' %
+        f.write('\t\tvar baseLayer = L.tileLayer(%s, {%s, mapid: "streets"});' %
                 (self.tile_url, self.attribution))
         f.write('\t\tllMap = L.map("mapid", {\n')
         f.write('\t\t\tzoomSnap: 0,\n')
