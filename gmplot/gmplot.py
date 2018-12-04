affaa8909c00add3e95e31515a2db1f82aea5530
Enrique Arriaga <enrique@stroly.jp>
2018-12-04 11:50:24 +0900

    fix attribution

--- gmplot/gmplot.py
+++ gmplot/gmplot.py
@@ -355,10 +355,10 @@ class GoogleMapPlotter(object):
 
     # TODO: Add support for mapTypeId: google.maps.MapTypeId.SATELLITE
     def write_map(self,  f):
+        f.write('\t\tattribution = "%s"' % (self.attribution.replace('"', "'")))
         f.write('\t\tvar baseLayer = L.tileLayer("%s", {\n' %
                 (self.tile_url))
-        f.write('\t\t\t"%s",\n\t\t\tmapid: "streets"});\n' %
-                (self.attribution.replace('"', "'")))
+        f.write('\t\t\t"attribution",\n\t\t\tmapid: "streets"});\n')
         f.write('\t\tllMap = L.map("mapid", {\n')
         f.write('\t\t\tzoomSnap: 0,\n')
         f.write('\t\t\tmaxZoom: 18\n')
